6c8e8ee510c8404fe14365f76b6a7a2e80e0b0e3
Jay Callicott <jaykali@42248.no-reply.drupal.org>
2014-04-09 09:34:39 -0500

    Updating documentation for API

--- autotag.api.php
+++ autotag.api.php
@@ -8,8 +8,20 @@
 /**
  * Autotag fetch terms.
  *
- * @param type $field_instance
- * @param type $search_text
+ * Modules will need to implement their own field settings (if needed)
+ * to pair with this hook.
+ *
+ * @see autotag_form_field_ui_field_edit_form_alter()
+ *
+ * @param object $field_instance
+ *   This contains the field instance object which will contain the settings
+ *   that modules implementing this hook needs to act upon.
+ * @param string $search_text
+ *   This contains the search string that will be examined to look up terms
+ *   against.
+ *
+ * @return array
+ *   List of term names found.
  */
 function hook_autotag_fetch_terms($field_instance, $search_text) {
 
