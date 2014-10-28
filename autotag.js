81cf72ecc345782587f8c2cc2b82e90164b2b9d4
Jay Callicott <jaykali@42248.no-reply.drupal.org>
2014-10-28 12:35:19 -0500

    Added support for more entities, other bug fixes

--- autotag.js
+++ autotag.js
@@ -7,47 +7,49 @@
 
   Drupal.behaviors.addAutoTag = {
     attach: function(context, settings) {
-      $('.autotag.form-text', context).each(function() {
-        var id = $(this).attr('id') + '-autotag';
+      if (context == document) {
+        $('.autotag.form-text', context).each(function() {
+          var id = $(this).attr('id') + '-autotag';
 
-        $(this).after('<input type="submit" class="autotag-submit" value="Autotag" id="' + id + '" />');
-      });
-
-      $('.autotag-submit').click(function(){
-
-        // Look for body field text.
-        // @todo support additional fields.
-        var search_text = '';
-
-        // Look for Wysiwyg
-        if (Drupal.wysiwyg.instances['edit-body-und-0-value'] != undefined) {
-          search_text = Drupal.wysiwyg.instances['edit-body-und-0-value'].getContent();
-        }
-        else {
-          search_text = $('#edit-body-und-0-value').text();
-        }
-
-        // Fetch terms for this field
-        var id = $(this).attr('id').replace('-autotag', '');
-
-        // Add throbber
-        $(this).after('<span class="ajax-progress ajax-progress-throbber"></span>');
-
-        var url = settings.basePath + 'autotag/' + settings.autotag.entity_type + '/' + settings.autotag.field_name + '/' + settings.autotag.bundle;
-
-        $.ajax({
-          url: url,
-          type: 'POST',
-          data: { search_text: search_text, current_terms: $('#' + id).val() },
-          context: context
-        })
-        .done(function(data) {
-          // Add terms to Autotag field
-          $('#' + id).val(data).parent().find('.ajax-progress').remove();
+          $(this).after('<input type="submit" class="autotag-submit form-submit" value="Autotag" id="' + id + '" />');
         });
 
-        return false;
-      });
+        $('.autotag-submit').click(function(){
+
+          // Look for body field text.
+          var search_text = '';
+          var source_field = settings.autotag.source_field;
+
+          // Look for Wysiwyg
+          if (typeof(Drupal.wysiwyg) !== 'undefined' && typeof(Drupal.wysiwyg.instances['edit-' + source_field + '-und-0-value']) !== 'undefined') {
+            search_text = Drupal.wysiwyg.instances['edit-' + source_field + '-und-0-value'].getContent();
+          }
+          else {
+            search_text = $('#edit-' + source_field + '-und-0-value').text();
+          }
+
+          // Fetch terms for this field
+          var id = $(this).attr('id').replace('-autotag', '');
+
+          // Add throbber
+          $(this).after('<span class="ajax-progress ajax-progress-throbber"></span>');
+
+          var url = settings.basePath + 'autotag/' + settings.autotag.entity_type + '/' + settings.autotag.field_name + '/' + settings.autotag.bundle;
+
+          $.ajax({
+            url: url,
+            type: 'POST',
+            data: { search_text: search_text, current_terms: $('#' + id).val() },
+            context: context
+          })
+          .done(function(data) {
+            // Add terms to Autotag field
+            $('#' + id).val(data).parent().find('.ajax-progress').remove();
+          });
+
+          return false;
+        });
+      }
     }
   };
 
